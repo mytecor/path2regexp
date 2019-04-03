345175533c7988a4639c89953fc63cd484bca5f8
Vlad Afonin <afonin.com123@gmail.com>
2019-04-03 23:16:06 +0300

    added support for unnamed parameters

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 
 module.exports = path => {
-	let params = []
 	let regexp = RegExp('^' + path.replace(/\/:(.*?)(\??)(?=$|\/)/g, (match, param, optional) => {
-		params.push(param)
-		return optional? '(?:\/([^\/]+?))?': '\/([^\/]+?)'
+		return optional? '(?:\/(?<' + param + '>.*))?': '\/(?<' + param + '>.*)'
 	}) + '(?:\/)?$', 'i')
 	return {
 		test: regexp.test,
 		exec: path => {
 			let match = regexp.exec(path)
 			if(!match) return null
-			let resParams = {}
-			params.forEach((param, i) => resParams[param] = match[i+1])
-			return resParams
+			delete match[0]
+
+			let res = []
+			for(let i in match.groups) {
+				delete match[match.indexOf(match.groups[i])]
+				res[i] = match.groups[i]
+			}
+
+			for(let i of match) if(i) res.push(i)
+			return res
 		}
 	}
 }
