1885c852b1e452280f18d284e92809a20684b007
Vlad Afonin <afonin.com123@gmail.com>
2019-07-15 03:33:15 +0300

    rewritten

--- README.md
+++ README.md
@@ -4,19 +4,20 @@
 ```js
 let p2r = require('path2regexp')
 
+// Optional parameters
 let route = p2r('/user/:login/:method?/:action')
-console.log(route('/user/Midnighcoder/test')) // [ login: 'Midnighcoder', method: undefined, action: 'test' ]
-console.log(route('/user/Midnighcoder/get/test')) // [ login: 'Midnighcoder', method: 'get', action: 'test' ]
+console.log(route('/user/Midnighcoder/test')) // {login: 'Midnighcoder', method: undefined, action: 'test'}
+console.log(route('/user/Midnighcoder/get/test')) // {login: 'Midnighcoder', method: 'get', action: 'test'}
 
 // Unnamed parameters
 let route2 = p2r('/user/(.*)/:action')
-console.log(route('/user/get/test')) // [ 'get', action: 'test' ]
+console.log(route('/user/get/test')) // {unnamed: ['get'], action: 'test'}
 
 // Regexp route
 let route3 = p2r(/\/user\/(.*)/)
-console.log(route('/user/get/test')) // [ 'get/test' ]
+console.log(route('/user/get/test')) // {unnamed: ['get/test']}
 
 // Other routes
 let route4 = p2r('/user/(get|post)/:action')
-console.log(route('/user/post/test')) // [ 'post', action: 'test' ]
+console.log(route('/user/post/test')) // {unnamed: ['post'], action: 'test'}
 ```
--- index.js
+++ index.js
@@ -1,57 +1,41 @@
 
 module.exports = route => {
-	let isRegexp, hasParams
+	let hasUnnamed = true, hasNamed
 
 	if(typeof route == 'string') {
-		isRegexp = /[^\\][\[\*\(]/.test(route)
-		hasParams = ~route.indexOf(':')
+		hasNamed = ~route.indexOf(':')
+		hasUnnamed = /[^\\][\[\*\(]/.test(route)
 
-		// Without regexp
-		if(!hasParams && !isRegexp)
-			return path => path == route? [] : null
-
+		if(!hasNamed && !hasUnnamed) return path => path == route? {named: {}, unnamed: []} : null
+		
 		route = new RegExp('^' + route.replace(/\/:(.*?)(\??)(?=$|\/)/g, (match, param, optional) => 
-			optional? '(?:/(?<' + param + '>[^/]*))?': '/(?<' + param + '>[^/]*)') + '/?$')
-	} else isRegexp = true
+			optional? '(?:/(?<' + param + '>[^/]*))?' : '/(?<' + param + '>[^/]*)') + '/?$')
+	}
 
-	// Unnamed parameters
-	if(isRegexp)
-		if(hasParams) return path => {
-			let match = route.exec(path)
+	if(hasNamed && hasUnnamed) return path => {
+		let match = route.exec(path)
+		if(!match) return null
 
-			if(!match) return null
-			if(match.length == 1) return []
-			if(match.groups) {
-				let res = []
+		// Named and unnamed parameters
+		let
+			res = match.groups,
+			vals = Object.values(match.groups),
+			unnamed = res.unnamed = []
 
-				let keys = Object.keys(match.groups)
-				let vals = Object.values(match.groups)
-
-				for(let i = 1; i < match.length; i++) {
-					let pos = vals.indexOf(match[i])
-					if(~pos) {
-						res[keys[pos]] = match[i]
-						delete vals[pos]
-					}
-					else res.push(match[i])
-				}
-
-				return res
-			}
-			return match.slice(1)
-		}
-		else return path => {
-			let match = route.exec(path)
-
-			if(!match) return null
-			if(match.length == 1) return []
-			return match.slice(1)
+		for(let i = 1; i < match.length; i++) {
+			let pos = vals.indexOf(match[i])
+			if(!~pos) unnamed.push(match[i])
+			delete vals[pos]
 		}
 
-	// Named parameters
+		return res
+	}
+	
 	return path => {
 		let match = route.exec(path)
 		if(!match) return null
-		return Object.setPrototypeOf(Object.defineProperty(match.groups, 'length', {value: 0, writable: true}), Array.prototype) || []
+
+		// Named or unnamed parameters
+		return match.groups || {unnamed: match.slice(1)}
 	}
 }
